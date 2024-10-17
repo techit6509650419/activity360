3cad8dbcf33f0fc3c683d41ef4dd717fefb9dd97
6509650419techit <techit.cha@dome.tu.ac.th>
2024-10-17 10:30:45 +0700

    test pass

--- src/__test__/math.test.js
+++ src/__test__/math.test.js
@@ -3,17 +3,17 @@ const math = require('../math');
 describe('Math function', () => {
    describe('Add Function', () => {
     test('add 1 + 2 to equal 3', () => {
-        expect(1 + 2).toBe(4);
+        expect(1 + 2).toBe(3);
     });
    });
    describe('Subtract Function', () => {
     test('add 2 - 2 to equal 2', () => {
-        expect(2 - 2).toBe(1);
+        expect(2 - 2).toBe(0);
     });
    });
    describe('Multipy Function', () => {
     test('add 2 * 2 to equal 2', () => {
-        expect(2 * 2).toBe(7);
+        expect(2 * 2).toBe(4);
     });
    });
 
